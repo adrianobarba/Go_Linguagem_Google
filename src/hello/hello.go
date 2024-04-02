ec15cd892f0e4d22fe8b7641f97b77f259f19d88
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-04-02 09:49:10 -0300

    tipo e variaveis

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+// quando não é atribuido valor na variavel ela inicia com zero ou vazia
+// não precisa declarar a palavra reservada "var" nem seu tipo
 func main() {
-	fmt.Println("Olá Mundo, Adriano!")
+	nome := "Adriano"
+	versao := 1.1
+	idade := 26
+	fmt.Println("Olá sr.", nome, "suar idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	//fmt.Println("O tipo da variavel versão é", reflect.TypeOf(versao))
 }
