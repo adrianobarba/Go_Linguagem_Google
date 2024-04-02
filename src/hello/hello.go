5620a319442fd98e41b90181482d0786b5449464
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-04-02 19:25:57 -0300

    requisição web

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,14 +4,40 @@ import (
 	"fmt"
 )
 
-// quando não é atribuido valor na variavel ela inicia com zero ou vazia
-// não precisa declarar a palavra reservada "var" nem seu tipo
 func main() {
 	nome := "Adriano"
 	versao := 1.1
-	idade := 26
-	fmt.Println("Olá sr.", nome, "suar idade é", idade)
+	fmt.Println("Olá Sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 
-	//fmt.Println("O tipo da variavel versão é", reflect.TypeOf(versao))
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var comando int
+	//fmt.Scanf("%d", &comando)
+	fmt.Scan(&comando)
+
+	fmt.Println("O comando escolhido foi", comando)
+
+	//if comando == 1 {
+	//	fmt.Println("Monitorando...")
+	//} else if comando == 2 {
+	//	fmt.Println("Exibindo Logs...")
+	//} else if comando == 0 {
+	//	fmt.Println("Saindo do programa")
+	//} else {
+	//	fmt.Println("Não conheço este comando")
+	//}
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+	default:
+		fmt.Println("Não conheço este comando")
+	}
 }
